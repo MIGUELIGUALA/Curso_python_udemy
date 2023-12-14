20935b1de49b4c69132bb20f2893747dcfb300ac
MIGUELIGUALA <148406875+MIGUELIGUALA@users.noreply.github.com>
2023-12-13 21:27:21 -0600

    Add files via upload

---
+++ EjercicioTupla1.py
@@ -0,0 +1,9 @@
+#Escribir una tupla con los meses del año, luego, pide al usuario un numero, el que haya ingresado, es el mes que debe mostrar en la tupla
+
+meses = ( "Enero" , "Febrero" , "Marzo" , "Abril" , "Mayo" , "Junio" , "Agosto" , "Septiembre" , "Octubre" , "Noviembre" , "Diciembre" )
+
+usuario = int(input("Ingrese un numero del 1 al 12: "))
+usuario =- 1
+
+
+print ("EL MES SELECCIONADO FUE: ", meses [usuario])
---
+++ EjercicioTupla2.py
@@ -0,0 +1,7 @@
+alphabet = ("a", "b", "c", "d", "e" , "f" , "g" , "h" , "i", "j", "k", "l", "m" , "n" ,
+             "o" , "p" , "q", "r", "s", "t", "u" , "w" , "x" , "y", "z")
+
+usuario = int(input("Ingrese un numero correspondiente a una letra del ABCdario: "))
+
+print ("La letra correspondiente a ese numero es: ",alphabet[usuario-1])
+
